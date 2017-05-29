8a1194225dcd779bbfe623444341e1748fbac438
Christine Dey <christinedey5@gmail.com>
2017-05-29 16:03:16 -0700

    Add Spotify API call

---
+++ server/api/spotify.js
@@ -0,0 +1,52 @@
+const Promise = require('bluebird');
+const request = Promise.promisify(require('request'));
+
+module.exports = {
+  searchArtist: function(artistName) {
+    let artistSearchQuery = artistName.name.split(' ').join('%20');
+    return new Promise(function(resolve, reject) {
+      let requestString = `api.spotify.com/v1/search?q=${artist}&type=artist&limit=1`;
+      request(requestString, function(error, response) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          resolve(getArtistInfo(response.body));
+        }
+      });
+    }).then(function(artistInfo) {
+      return new Promise(function(resolve, reject) {
+        let requestString = `api.spotify.com/v1/artists/{artistInfo.id}/top-tracks?country=US`;
+        request(requestString, function(error, response) {
+          if (error) {
+            console.log(error);
+            reject(error);
+          } else {
+            artistInfo.tracks = getTrackInto(response.body);
+            resolve(artistInfo);
+          }
+        });
+      });
+    });
+  };
+};
+
+let getArtistInfo = apiResult => {
+  let artist = JSON.parse(apiResult).artists;
+  return {
+    spotify_artist_id: artist.id,
+    artist_name: artist.name,
+    artist_url: artist.images[1].url
+  };
+};
+
+let getTrackInfo = apiResult => {
+  let tracks = JSON.parse(apiResult).tracks;
+  return tracks.map(track => {
+    return {
+      track_name: track.name,
+      album: track.album.name,
+      track_url: track.images[2].url
+    };
+  });
+};
